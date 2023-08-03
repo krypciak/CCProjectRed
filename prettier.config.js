91344b34e41850210e9c6e7b84f758d945ebbd0d
Alyxia Sother <alyxia@riseup.net>
2023-08-03 12:47:57 +0200

    [Meta] FINALLY fix the linter config, fixing CI

--- prettier.config.js
+++ prettier.config.js
@@ -1,3 +1,5 @@
 module.exports = {
+  // Despite the eslint config specifying that this rule is disabled, it still triggers.
+  // eslint-disable-next-line node/no-unpublished-require
   ...require('eslint-config-dmitmel/prettier.config.js'),
 };
