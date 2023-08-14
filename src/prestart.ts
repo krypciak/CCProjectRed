66d91bc7fd4ae9c371589f63cb5ee6d19073ea41
Alyxia Sother <alyxia@riseup.net>
2023-08-14 22:08:39 +0200

    [prestart] Disable the title screen overrides

--- src/prestart.ts
+++ src/prestart.ts
@@ -1,8 +1,13 @@
 // Patches to internals {{{
-import './patches/crosscode.js';
-import './patches/new-game.js';
+    import './patches/crosscode.js';
+    import './patches/new-game.js';
 // }}}
 
 // UI stuff {{{
-import './ui/title-screen.js';
+    // This one isn't really needed right now... we already hook into the new
+    // game plus menu and the "start game" text doesn't really have to be
+    // overridden
+    // Keeping for posterity though! The injections might be helpful for readers
+    // of the source code
+    // import './ui/title-screen.js';
 // }}}
